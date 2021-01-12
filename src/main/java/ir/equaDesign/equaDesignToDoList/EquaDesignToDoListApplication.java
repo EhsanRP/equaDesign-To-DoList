a12613595995c0f631d22134a959c266c6d3aa2b
EhsanRP <sir.ehrp@gmail.com>
2021-01-12 23:52:58 +0330

    Project Init

---
+++ src/main/java/ir/equaDesign/equaDesignToDoList/EquaDesignToDoListApplication.java
@@ -0,0 +1,13 @@
+package ir.equaDesign.equaDesignToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquaDesignToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquaDesignToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/equaDesign/equaDesignToDoList/EquaDesignToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.equaDesign.equaDesignToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquaDesignToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
